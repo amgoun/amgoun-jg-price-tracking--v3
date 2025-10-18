c2105be4e6bea518fd7b9274717b64b11b1b13b2
amgoun studio <amgoundesign02@gmail.com>
2025-10-18 22:52:11 +0100

    update cta img

--- components/cta-section.tsx
+++ components/cta-section.tsx
@@ -17,7 +17,7 @@ interface CTASectionProps {
 
 export default function CTASection({ cta }: CTASectionProps) {
   return (
-    <section className="py-20 px-6 lg:px-12 bg-blacker">
+    <section className=" pt:5 md:py-8 px-6 lg:px-12 bg-blacker">
       <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
